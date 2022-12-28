b873a3e4a589823e0c5d0fe50353c215087a7987
Anca G <anca.gherman2013@gmail.com>
2022-12-28 18:12:45 +0200

    Initial commit

---
+++ src/WaterSource.java
@@ -0,0 +1,8 @@
+class WaterSource {
+    private String s;
+    WaterSource() {
+        System.out.println("WaterSource()");
+        s = "Constructed";
+    }
+    public String toString() { return s; }
+}
